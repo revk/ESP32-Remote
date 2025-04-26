a6037272a17ba1fd0284ac9d65875facaf58f91c
RevK <github@k.gg>
2025-04-26 09:58:38 +0100

    Start base app

--- main/Remote.c
+++ main/Remote.c
@@ -14,9 +14,22 @@ const char TAG[] = "Remote";
 #include "led_strip.h"
 #include "bleenv.h"
 #include "halib.h"
-
+#include <lwpng.h>
 
 httpd_handle_t webserver = NULL;
+SemaphoreHandle_t epd_mutex = NULL;
+
+static void *
+my_alloc (void *opaque, uInt items, uInt size)
+{
+   return mallocspi (items * size);
+}
+
+static void
+my_free (void *opaque, void *address)
+{
+   free (address);
+}
 
 const char *
 app_callback (int client, const char *prefix, const char *target, const char *suffix, jo_t j)
@@ -69,19 +82,83 @@ register_get_uri (const char *uri, esp_err_t (*handler) (httpd_req_t * r))
    register_uri (&uri_struct);
 }
 
+#ifdef	CONFIG_LWPNG_ENCODE
+static esp_err_t
+web_frame (httpd_req_t * req)
+{
+   xSemaphoreTake (epd_mutex, portMAX_DELAY);
+   uint8_t *png = NULL;
+   size_t len = 0;
+   uint32_t w = gfx_raw_w ();
+   uint32_t h = gfx_raw_h ();
+   uint8_t *b = gfx_raw_b ();
+   const char *e = NULL;
+   if (gfx_bpp () == 16)
+   {                            // RGB packed
+      uint8_t *buf = mallocspi (w * 3);
+      if (!buf)
+         e = "malloc";
+      else
+      {
+         lwpng_encode_t *p = lwpng_encode_rgb (w, h, &my_alloc, &my_free, NULL);
+         if (b)
+            while (h--)
+            {
+               for (int x = 0; x < w; x++)
+               {
+                  uint16_t v = (b[x * 2 + 0] << 8) | b[x * 2 + 1];
+                  uint8_t r = (v >> 11) << 3;
+                  r += (r >> 5);
+                  uint8_t g = ((v >> 5) & 0x3F) << 2;
+                  g += (g >> 6);
+                  uint8_t b = (v & 0x1F) << 3;
+                  b += (b >> 5);
+                  buf[x * 3 + 0] = r;
+                  buf[x * 3 + 1] = g;
+                  buf[x * 3 + 2] = b;
+               }
+               lwpng_encode_scanline (p, buf);
+               b += w * 2;
+            }
+         e = lwpng_encoded (&p, &len, &png);
+         free (buf);
+      }
+   }
+   ESP_LOGD (TAG, "Encoded %u bytes %s", len, e ? : "");
+   if (e)
+   {
+      revk_web_head (req, *hostname ? hostname : appname);
+      revk_web_send (req, e);
+      revk_web_foot (req, 0, 1, NULL);
+   } else
+   {
+      httpd_resp_set_type (req, "image/png");
+      httpd_resp_send (req, (char *) png, len);
+   }
+   free (png);
+   xSemaphoreGive (epd_mutex);
+   return ESP_OK;
+}
+#endif
+
 static esp_err_t
 web_root (httpd_req_t * req)
 {
    if (revk_link_down ())
       return revk_web_settings (req);   // Direct to web set up
    revk_web_head (req, *hostname ? hostname : appname);
-
+#ifdef	CONFIG_LWPNG_ENCODE
+   revk_web_send (req, "<p><img src=frame.png></p>");
+   revk_web_send (req, "<p><a href=/>Reload</a></p>");
+#endif
    return revk_web_foot (req, 0, 1, NULL);
 }
 
 void
 app_main ()
 {
+   epd_mutex = xSemaphoreCreateMutex ();
+   xSemaphoreGive (epd_mutex);
    revk_boot (&app_callback);
    revk_start ();
 
